acedf3e9d92b8fbce6e2da5dd21b3d55a8f6a6e9
Azhar Nian <azharnian@gmail.com>
2022-03-07 09:36:16 +0700

    index Html

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,14 @@
 module.exports = {
   apps : [{
     name   : "wanian-01",
-    script : "./app.js"
+    script : "./app.js",
+    instances: 2,
+        exec_mode: "cluster",
+        watch: true,
+        increment_var : 'PORT',
+        env: {
+            "PORT": 3000,
+            "NODE_ENV": "development"
+        }
   }]
 }
